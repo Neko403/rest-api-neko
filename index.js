f80319485f5a8b737315971f697ef9992e60491f
Neko403 <nenekori@protonmail.com>
2025-10-23 09:05:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.set("json spaces", 2);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/', express.static(path.join(__dirname, '../api-page')));
-app.use('/src', express.static(path.join(__dirname, '../src')));
+app.use('/', express.static(path.join(__dirname, './api-page')));
+app.use('/src', express.static(path.join(__dirname, './src')));
 
-const settingsPath = path.join(__dirname, '../src/settings.json');
+const settingsPath = path.join(__dirname, './src/settings.json');
 const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf-8'));
 global.apikey = settings.apiSettings.apikey;
 
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 
 // Load API Routes
 let totalRoutes = 0;
-const apiFolder = path.join(__dirname, '../src/api');
+const apiFolder = path.join(__dirname, './src/api');
 if (fs.existsSync(apiFolder)) {
   fs.readdirSync(apiFolder).forEach((subfolder) => {
     const subfolderPath = path.join(apiFolder, subfolder);
@@ -63,16 +63,16 @@ console.log(chalk.bgHex('#90EE90').hex('#333').bold(' Load Complete! ✓ '));
 console.log(chalk.bgHex('#90EE90').hex('#333').bold(` Total Routes Loaded: ${totalRoutes} `));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../api-page/index.html'));
+  res.sendFile(path.join(__dirname, './api-page/index.html'));
 });
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '../api-page/404.html'));
+  res.status(404).sendFile(path.join(__dirname, './api-page/404.html'));
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).sendFile(path.join(__dirname, '../api-page/500.html'));
+  res.status(500).sendFile(path.join(__dirname, './api-page/500.html'));
 });
 
 // Tidak ada app.listen() di Vercel
